0dd3a803d9052b43b366fcbb22a403a35f6a2386
MaximZamotaev228 <112868632+MaximZamotaev228@users.noreply.github.com>
2022-10-31 11:08:24 +0300

    Update JavScr3.js

--- JavScr3.js
+++ JavScr3.js
@@ -1,1 +1,20 @@
-1
+jQuery(function ($) {
+    $(document).ready(function () {
+        $(".slider").slick({
+            arrows: true,
+            dots: true,
+            responsive: [
+                {
+                    breakpoint: 960,
+                    settings: {
+                        arrows: true,
+                        dots: true,
+                        slidesToShow: 2
+                    }
+                }
+            ],
+            slidesToScroll: 1,
+            slidesToShow: 4
+        });
+    });
+});
